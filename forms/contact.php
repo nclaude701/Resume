8c27d74f6e2a7bebf241c632e9e3c2259920df0f
nclaude701 <118156351+nclaude701@users.noreply.github.com>
2023-11-01 06:38:30 -0700

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,46 +1,31 @@
 <?php
-/**
- * Requires the "PHP Email Form" library
- * The "PHP Email Form" library is available only in the pro version of the template
- * The library should be uploaded to: vendor/php-email-form/php-email-form.php
- * For more info and help: https://bootstrapmade.com/php-email-form/
- */
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Replace with your email address
+    $receiving_email_address = 'nclaude701@gmail.com';
 
-// Replace contact@example.com with your real receiving email address
-$receiving_email_address = 'nclaude701@gmail.com';
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-// Check if the PHP Email Form library file exists
-if (file_exists($php_email_form = '../assets/vendor/php-email-form/validate.js')) {
-    include($php_email_form);
+    $to = $receiving_email_address;
+    $subject = 'New Contact Form Submission: ' . $subject;
+    $headers = "From: $email";
+    $message = "You have received a new message from the contact form on your website.\n\n";
+    $message .= "Name: $name\n";
+    $message .= "Email: $email\n";
+    $message .= "Subject: $subject\n\n";
+    $message .= "Message:\n$message";
+
+    if (mail($to, $subject, $message, $headers)) {
+        // Email sent successfully
+        echo '1';
+    } else {
+        // Email sending failed
+        echo '0';
+    }
 } else {
-    die('Unable to load the "PHP Email Form" Library!');
+    // If the script is accessed directly, return an error message
+    echo 'Direct access is not allowed.';
 }
-
-$contact = new PHP_Email_Form;
-$contact->ajax = true;
-
-$contact->to = $receiving_email_address;
-
-// Make sure to sanitize and validate user inputs before using them
-$contact->from_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-$contact->from_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$contact->subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
-$contact->message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-
-// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-/*
-$contact->smtp = array(
-  'host' => 'example.com',
-  'username' => 'example',
-  'password' => 'pass',
-  'port' => '587'
-);
-*/
-
-$contact->add_message($_POST['name'], 'From');
-$contact->add_message($_POST['email'], 'Email');
-$contact->add_message($_POST['subject'], 'Email');
-$contact->add_message($_POST['message'], 'Message', 10);
-
-echo $contact->send();
 ?>
